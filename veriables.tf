4d99092f11f012a7659091a2c35db2ad201cae7f
Vish-wassn <88095211+Vish-wassn@users.noreply.github.com>
2021-08-01 12:15:36 +0530

    Update veriables.tf

--- veriables.tf
+++ veriables.tf
@@ -1,29 +1,29 @@
 variable "aws_region"{
   description ="The AWS region to create things in."
-  region     ="us-east-2"
+  default    ="us-east-2"
 }
 
 variable "key_name"{
   description ="SSH keys to connect to ec2 instance"
-  key_name    ="new1"
+  default     ="new1"
 }
 
 variable "instance_type"{
   description ="instance type of ec2"
-  instance_type    ="t2.micro"
+  default    ="t2.micro"
 }
 
 variable "security_group"{
   description ="Name of the security group"
-  security_groups     ="security_jenkins_grp"
+  default     ="my-jenkins-security-group"
 }
 
 variable "tag_name"{
   description ="Tag name for ec2 instance"
-  tags   ="my-ec2-instance"
+  default   ="my-ec2-instance"
 }
 
 variable "ami_id"{
   description ="AMI for ubuntu ec2 instance"
-   ami      ="ami-0b9064170e32bde34"
+ default     ="ami-0b9064170e32bde34"
 }
