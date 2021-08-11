d347723e6db04c1f7df86b2c3ac4525c841751c0
Vish-wassn <88095211+Vish-wassn@users.noreply.github.com>
2021-08-11 19:22:30 +0530

    Update veriables.tf

--- veriables.tf
+++ veriables.tf
@@ -2,18 +2,14 @@ variable "aws_region" {
   description = "Region for the VPC"
   default = "us-east-2"
 }
-
 variable "my_vpc1" {
   description = "CIDR for the VPC"
   default = "10.0.0.0/16"
 }
-
 variable "p_subnet12" {
   description = "CIDR for the public subnet"
   default = "10.0.1.0/24"
 }
-
-
 variable "my_vpc_secure" 
   description = "security group"
   default = "my_vpcsecure"
@@ -22,7 +18,6 @@ variable "ami_id"{
   description = "ec2 instance "
    default     ="ami-0b9064170e32bde34"
 }
-
 variable "instance_type"{
   description ="instance type of ec2"
   default    ="t2.micro"
