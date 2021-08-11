fd00ca01f2a843becb3fff09a67365494bac9a66
Vish-wassn <88095211+Vish-wassn@users.noreply.github.com>
2021-08-11 20:46:11 +0530

    Update veriables.tf

--- veriables.tf
+++ veriables.tf
@@ -10,6 +10,10 @@ variable "p_subnet12" {
   description = "CIDR for the public subnet"
   default = "10.0.1.0/24"
 }
+variable "private_subnet" {
+  description = "CIDR for the private subnet"
+  default = "10.0.2.0/24"
+}
 variable "my_vpc_secure" {
  description = "security group"
   default = "my_vpcsecure"
