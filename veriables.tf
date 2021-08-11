377de385a5637efeed9310838a2b4a62f8c94ca8
Vish-wassn <88095211+Vish-wassn@users.noreply.github.com>
2021-08-11 19:46:20 +0530

    Update veriables.tf

--- veriables.tf
+++ veriables.tf
@@ -11,7 +11,7 @@ variable "p_subnet12" {
   default = "10.0.1.0/24"
 }
 variable "my_vpc_secure" 
-  description = "security group"
+ # description = "security group"
   default = "my_vpcsecure"
 }
 variable "ami_id"{
