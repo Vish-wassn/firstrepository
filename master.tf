34da393846f3b0e904c6d178248818a8acfef3ea
Vish-wassn <88095211+Vish-wassn@users.noreply.github.com>
2021-08-11 20:57:31 +0530

    Update master.tf

--- master.tf
+++ master.tf
@@ -46,12 +46,24 @@ resource "aws_internet_gateway" "i-gw" {
 
 # Create nat Gateway and Attach it to VPC
 resource "aws_nat_gateway" "nat-gw" {
+allocation_id = aws_eip.myFirstInstance.id
  vpc_id = aws_vpc.my_vpc.id
 connectivity_type = "private"
 subnet_id="${aws_subnet.p_subnet1.id}"
   tags = {
     Name = "nat_igw"
   }
+}
+
+
+resource "aws_eip" "myFirstInstance" {
+   vpc  =true
+ 
+instance = aws_instance.myFirstInstance.id
+ tags= {
+    Name = "my_elastic_ip"
+  }
+
 }
 
 # Create Route Table and Add Public Route
